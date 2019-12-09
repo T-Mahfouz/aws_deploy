2fec5c5c16b69a186ff32adb9c2add53cba7e3f2
Rabiya Babar <rabiyababar64@gmail.com>
2019-12-09 16:48:36 +0500

    "cross icon on form errors"

--- app/assets/javascripts/contact.js
+++ app/assets/javascripts/contact.js
@@ -1,11 +1,16 @@
 
 $(document).ready(function() {
-    $(".submit").on('click',(function() {
+    $('#new_booking').on('ajax:success', function(e, data, status, xhr){
         $(".submit").addClass("loading");
         setTimeout(function() {
             $(".submit").addClass("hide-loading");
             // For failed icon just replace ".done" with ".failed"
-            $(".done").addClass("finish");
+            if ($('.notice').length > 0) {
+                $(".failed").addClass("finish");
+            }
+            else if($('.notice').length == 0)  {
+                $(".done").addClass("finish");
+            }
         }, 3000);
         setTimeout(function() {
             $(".submit").removeClass("loading");
@@ -13,5 +18,9 @@ $(document).ready(function() {
             $(".done").removeClass("finish");
             $(".failed").removeClass("finish");
         }, 5000);
-    }));
+    });
 });
+
+
+
+
