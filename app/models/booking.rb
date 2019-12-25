4b88caa562cf7166c1d02dd1847ff25c0911b610
Muhammad Umair Raza <umair.raza101@gmail.com>
2019-12-25 17:08:53 +0500

    "email/phone field fixed"

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,7 +3,7 @@ class Booking < ApplicationRecord
   validates :second_name, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence:   true, length: { maximum: 255 },
-            format:     { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+            format:     { with: VALID_EMAIL_REGEX }
   validates :phone, presence: true
   validates :disease_type, presence: true
   validates :inquiry, presence: true
